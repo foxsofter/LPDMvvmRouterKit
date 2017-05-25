cd048d7ba45deadc058c65eef5ca4d1d91f63bd3
YDZ <ydz627@gmail.com>
2017-05-25 19:40:36 +0800

    暴露头文件

--- LPDMvvmRouterKit/Classes/LPDMvvmRouterKit.h
+++ LPDMvvmRouterKit/Classes/LPDMvvmRouterKit.h
@@ -22,3 +22,4 @@ FOUNDATION_EXPORT const unsigned char LPDMvvmRouterVersionString[];
 #import "LPDEvent.h"
 #import "LPDModuleProtocol.h"
 #import "LPDRouteURL.h"
+#import "UIViewController+LPDFinder"
