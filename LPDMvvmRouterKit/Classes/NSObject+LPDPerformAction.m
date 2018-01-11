5267f8775d0e987b29c608892cda7a9f0af74d98
fengming.shi <fengming.shi@ele.me>
2018-01-11 14:19:29 +0800

    fix: fix number type bug

--- LPDMvvmRouterKit/Classes/NSObject+LPDPerformAction.m
+++ LPDMvvmRouterKit/Classes/NSObject+LPDPerformAction.m
@@ -150,9 +150,13 @@ static inline void getTypeValueByNSValue(NSValue *inValue, const char *typeCode,
         } else if ([[NSString stringWithUTF8String:ivarType] containsString:@"NSString"]) {
           object_setIvar(self, ivars[i], ivarValue);
         } else if (strlen(cNumberTypes) == 1 && isCharArrayContains(ivarType, cNumberTypes)) {
-          NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
-          NSNumber *numberValue = [formatter numberFromString:ivarValue];
-          [self setValue:numberValue forKey:key];
+            if ([ivarValue isKindOfClass:[NSNumber class]]) {
+                [self setValue:ivarValue forKey:key];
+            } else {
+                NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+                NSNumber *numberValue = [formatter numberFromString:ivarValue];
+                [self setValue:numberValue forKey:key];
+            }
         } else {
           object_setIvar(self, ivars[i], ivarValue);
         }
